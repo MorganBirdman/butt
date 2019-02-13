2ae5825ce652c76f4740211f6dea00565af96ce3
Admin <Admin@DESKTOP-FKO6PH7>
2019-02-13 16:51:10 +0700

    ConsoleApp1

--- Program.cs
+++ Program.cs
@@ -4,15 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Mi y tu amo Alejandro");
-            Console.WriteLine("They never thanks me for opening doors");
-            Console.WriteLine("Welcome to da club buddy");
             Console.ReadKey();
         }
     }
