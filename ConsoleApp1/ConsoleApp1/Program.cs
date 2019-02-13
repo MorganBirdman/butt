66be03b62fe4e43e28eeb712c102406624506b17
Admin <Admin@DESKTOP-FKO6PH7>
2019-02-13 16:17:22 +0700

    message

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi y tu amo Alejandro");
+            Console.WriteLine("They never thanks me for opening doors")
+            Console.ReadKey();
+        }
+    }
+}
